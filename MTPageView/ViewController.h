0216d76c499d4c76f76ee32bcec428f73df3aaa2
yimouleng <wmt443040748@163.com>
2015-11-30 16:37:47 +0800

    Initial Commit

---
+++ MTPageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTPageView
+//
+//  Created by Eli on 15/11/30.
+//  Copyright © 2015年 Ely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MTPageView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MTPageView
+//
+//  Created by Eli on 15/11/30.
+//  Copyright © 2015年 Ely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
